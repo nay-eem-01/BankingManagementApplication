4b828c3467aa231da2d54851a055b3a7db44bd78
nayeem <ahmedmdnayeem05@gmail.com>
2025-10-19 12:57:45 +0600

    refactor: Remove password field from UserDto

--- src/main/java/org/example/bankingManagementApplication/dto/UserDto.java
+++ src/main/java/org/example/bankingManagementApplication/dto/UserDto.java
@@ -14,9 +14,6 @@ public class UserDto {
 
     @NotBlank(message = "Name can't be blank")
     private String name;
-    @NotBlank(message = "Password can't be blank")
-    @Size(min = 4, max = 8, message = "Password should be more than contain minimum 4 and maximum 8 characters")
-    private String password;
     @NotBlank
     @Email(message = "Invalid email format")
     private String email;
